c738c5d6952c5f18c55bf241bc1ad7374bf959c0
Allan Wazacz <cazwazacz@gmail.com>
2017-12-05 11:52:17 +0000

    create methods to check instance of and array includes

--- assert.js
+++ assert.js
@@ -2,6 +2,22 @@ var assert = {
   isTrue: function(assertionToCheck) {
       if(!assertionToCheck) {
         throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+      } else {
+        console.log("Assertion passed: " + assertionToCheck + " is truthy")
       }
+  },
+  isInstanceOf: function(object, objectType) {
+    if(!object instanceof objectType) {
+      throw new Error(`Assertion failed: ${object.constructor.name} is not a type of ${objectType.name}`);
+    } else {
+      console.log(`Assertion passed: Object is a type of ${objectType.name}`)
+    }
+  },
+  arrayIncludes: function(array, value) {
+    if(!array.includes(value)) {
+      throw new Error(`Assertion failed: Array does not include ${value}`)
+    } else {
+      console.log(`Assertion passed: Array includes ${value}`)
+    }
   }
 };
